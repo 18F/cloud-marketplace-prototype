598c2fa1b67f388e462b3b8f6934623277be6463
Atul Varma <varmaa@gmail.com>
2017-10-25 09:53:32 -0400

    More docker-related files.

---
+++ update.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+set -e
+
+# Note that much of the following will eventually be uncommented.
+
+#echo "----- Updating Python Dependencies -----"
+#python -m pip install -r requirements-dev.txt
+
+echo "----- Updating Node Dependencies -----"
+yarn
+
+#echo "----- Migrating Database -----"
+#python manage.py migrate --noinput
+
+#echo "----- Initializing Groups -----"
+#python manage.py initgroups
+
+#if [ -n "${APP_IS_ON_DOCKER_IN_CLOUD}" ]; then
+#  echo "----- Building Static Assets -----"
+#  gulp build
+#fi

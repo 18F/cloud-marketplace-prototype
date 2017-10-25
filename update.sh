d1ebc16996fd841c4a4042c4ae0cd70cebdb662b
Atul Varma <varmaa@gmail.com>
2017-10-25 12:30:58 -0400

    Migrate db in update.sh.

--- update.sh
+++ update.sh
@@ -8,13 +8,8 @@ python -m pip install -r requirements-dev.txt
 echo "----- Updating Node Dependencies -----"
 yarn
 
-# Note that much of the following will eventually be uncommented.
-
-#echo "----- Migrating Database -----"
-#python manage.py migrate --noinput
-
-#echo "----- Initializing Groups -----"
-#python manage.py initgroups
+echo "----- Migrating Database -----"
+python manage.py migrate --noinput
 
 #if [ -n "${APP_IS_ON_DOCKER_IN_CLOUD}" ]; then
 #  echo "----- Building Static Assets -----"

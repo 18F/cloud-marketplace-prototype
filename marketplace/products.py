7b9e6f6c108cf22d145ed9b9b34c99ae596fd072
Atul Varma <varmaa@gmail.com>
2017-10-26 10:22:16 -0400

    Add descriptions for products.

--- marketplace/products.py
+++ marketplace/products.py
@@ -15,23 +15,47 @@ class Product(metaclass=abc.ABCMeta):
     # Subclasses must define these.
     slug = None
     name = None
+    description = None
 
 
 class Favro(Product):
     slug = 'favro'
     name = 'Favro'
 
+    description = """
+        Favro is a planning and collaboration app which enables
+        developers, designers, and clients to all stay on the same
+        page and track progress.
+    """
+
 
 class Mural(Product):
     slug = 'mural'
     name = 'Mural'
+
+    description = """
+        A web-based solution for small teams that need a virtual
+        workspace in which they can brainstorm, plan and collaborate.
+    """
 
 
 class Trello(Product):
     slug = 'trello'
     name = 'Trello'
 
+    description = """
+        This is a card-based tool for managing projects and tasks.
+        It uses customizable Kanban-style boards, and offers
+        "power ups" such as GitHub integration.
+    """
+
 
 class Zoom(Product):
     slug = 'zoom'
     name = 'Zoom'
+
+    description = """
+        This is a video conferencing tool with features such as chat,
+        screeen sharing, and session recording. No account is required
+        to participate in conferences.
+    """

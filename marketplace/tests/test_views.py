50a185093886bf01aa886f01c08e4d62cf1cce2e
Atul Varma <varmaa@gmail.com>
2017-11-03 18:44:40 -0400

    Add some view tests.

---
+++ marketplace/tests/test_views.py
@@ -0,0 +1,41 @@
+import pytest
+
+from . import factories
+
+
+@pytest.mark.django_db
+def test_homepage_contains_products(client):
+    factories.TrelloFactory.create()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Trello' in response.content
+
+
+@pytest.mark.django_db
+def test_product_pages_exist(client):
+    factories.TrelloFactory.create()
+    response = client.get('/products/trello/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_nonexistent_product_pages_return_404(client):
+    response = client.get('/products/zzz/')
+    assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_logged_in_users_see_their_email_address(client):
+    user = factories.UserFactory.create(email='foo.bar@gsa.gov')
+    client.force_login(user)
+    response = client.get('/')
+    assert b'foo.bar@gsa.gov' in response.content
+
+
+@pytest.mark.django_db
+def test_logout_works(client):
+    user = factories.UserFactory.create(email='foo.bar@gsa.gov')
+    client.force_login(user)
+    response = client.get('/logout')
+    assert response.status_code == 200
+    assert b'foo.bar@gsa.gov' not in response.content
